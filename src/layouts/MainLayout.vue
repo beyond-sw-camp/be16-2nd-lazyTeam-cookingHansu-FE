91327ff27ae2fb7469ba6dca4fdd98c7616654a8
chaserChoi <chlwogur2010@gmail.com>
2025-07-21 17:24:44 +0900

    feat: 헤더 구현

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,14 @@
+<!-- 임시 레이아웃 -->
+<template>
+  <v-app>
+    <Header />
+    <v-main>
+      <v-container fluid class="pa-4" style="padding-top:80px;">
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+<script setup>
+import Header from '@/components/Header.vue'
+</script> 
