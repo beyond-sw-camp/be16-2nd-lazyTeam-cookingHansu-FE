34ec1f006486aaf7652022996e8fe330dee08b99
SeungJi20 <lina6709@naver.com>
2025-07-26 20:26:12 +0900

    feat: RecipeMainPage 기본 레이아웃 및 라우팅 추가 #14

---
+++ src/views/home/RecipeMainPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="recipe-main-page">
+    <Header />
+    <!-- 상단 탭 -->
+    <div class="nav-tabs">
+      <button :class="{ active: currentTab === 'recipe' }" @click="currentTab = 'recipe'">
+        레시피 게시글
+      </button>
+      <button :class="{ active: currentTab === 'lecture' }" @click="goToLecture">
+        강의 목록
+      </button>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+  name: "RecipeMainPage",
+  components: { Header, Footer },
+  data() {
+    return { currentTab: "recipe" };
+  },
+  methods: {
+    goToLecture() {
+      this.$router.push({ name: "LectureList" });
+    },
+  },
+};
+</script>
